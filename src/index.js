c5d65ee9a0d0f7e52edcf07bfdda951af403484a
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2024-06-27 14:18:27 +0100

    Generate all of dist, including html file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './image.jpeg';
+import printMe from './print.js';
 
 function component() {
-  const element = document.createElement('div');
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
+  const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  const myImage = new Image();
-  myImage.src = Icon;
-  element.appendChild(myImage);
+  element.appendChild(btn);
 
   return element;
 }
